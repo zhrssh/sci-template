603233f3d44940860c95dc7dc5840fc5dc3bc13b
zhrssh <98146850+zhrssh@users.noreply.github.com>
2024-04-24 18:48:04 +0800

    [feat] Added globals.py

---
+++ src/constants/globals.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+# Directories
+PROJECT_DIR = Path(*Path(__file__).parts[:-3])
+REPORTS_DIR = Path(PROJECT_DIR, "reports")
+MODELS_DIR = Path(PROJECT_DIR, "models")
+
+# Dataset Directories
+RAW_DATASET_DIR = Path(PROJECT_DIR, r"data\raw")
+PROCESSED_DATASET_DIR = Path(PROJECT_DIR, r"data\processed")
+EXTERNAL_DATASET_DIR = Path(PROJECT_DIR, r"data\external")
